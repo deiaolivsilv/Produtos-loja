7a1887783397edc7801bd0b0091bb421f2e3973c
deia <deiaolivsilv@gmail.com>
2026-06-11 16:16:31 -0300

    Alterações de terça

--- src/pages/Produto.jsx
+++ src/pages/Produto.jsx
@@ -6,6 +6,7 @@ function Produto(){
     const [titulo, setTitulo] = useState()
     const [foto, setFoto] = useState()
     const [preco, setPreco] =useState()
+    const [ produto, setProduto] = useState({})
 
  const {id} = useParams()
 
@@ -14,6 +15,7 @@ function Produto(){
             async function buscarDados(){
                 const resposta = await fetch('https://dummyjson.com/products/'+id)
                 const dados = await resposta.json()
+                setProduto(dados)
                 setTitulo(dados.title)
                 setPreco(dados.price)
                 setFoto(dados.images)
@@ -25,11 +27,16 @@ function Produto(){
 
     return <>
     <h2>{titulo}</h2>
-    <img src={foto} />
+    <img src={foto}/>
+
 
     <p>Preço do produto: {preco}</p>
-    <p >"description": "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",</p>
+    <p >{produto.description} </p>
     </>
+
+
+
+
 }
 
 
